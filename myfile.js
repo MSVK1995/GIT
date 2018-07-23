1228d7bfd24a9d50e808b6535bfe3a47b937921d
M.S.Vikas Kumar <vikas.kumar4@globallogic.com>
2018-07-23 19:59:11 +0530

    Added Hello string again

--- myfile.js
+++ myfile.js
@@ -28,7 +28,7 @@ Mkt3L10n.lobEditor = {
     your_noteson_this_form: "your notes on this form",
     How_the_labels_and_fields_are_arranged_in_this_form: "How the labels and fields are arranged in this form",
     Discount: "Discount",
-    Hello: "Hello", 
+    Hello: "Hello",
     Files: "Files"
 };  
 
